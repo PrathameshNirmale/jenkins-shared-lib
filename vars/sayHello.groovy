eda9d74e281ae5ccdb49584a373a673935747cb3
PrathameshNirmale <79807692+PrathameshNirmale@users.noreply.github.com>
2026-01-30 19:30:02 +0530

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,8 +1,7 @@
 import org.example.Helper
 
 def call(String name = 'there') {
-    // Pass 'this' so the helper can access Jenkins DSL steps
-    def greeting = Helper.readGreeting(this) 
+    def greeting = libraryResource('org/example/greeting.txt').trim()
     def upperName = Helper.toUpper(name)
     echo "${greeting} ${upperName}!"
 }
